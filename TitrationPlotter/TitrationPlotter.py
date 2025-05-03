0c8fd50e503a8bfedd400f1bd1d46ed130c8427a
taira <taira@THINKPADX13>
2025-05-03 17:30:37 +0900

    プロジェクト ファイルを追加します。

---
+++ TitrationPlotter/TitrationPlotter.py
@@ -0,0 +1,28 @@
+﻿import numpy as np
+import scipy.optimize as opt
+
+ca = 0.0
+cb = 0.0
+va = 0.0
+vb = 0.0
+kw = 1e-14
+
+def titration_pH (vb: np.ndarray, ka: float) -> np.ndarray:
+    '''This returns the array of the roots.
+
+    This function solves the equation about the concentration of proton.
+
+    The meaningd of the paramaters are:
+
+    ka: Ka value of the titrated acid.
+    ca: initial concentration of the concentration of the titrated acid.
+    va: Initial volume of the acid.
+    cb: initial concentration of the concentration of the titrating base.
+    vb: volume of added base.
+    kw: Kw value at the tempreture.
+    '''
+    # For each vb, make and solve the equation
+    for arr in np.nditer(vb):
+        equation = np.poly1d([1, ka + (vb * cb) / (va + vb), ((ka / (va + vb)) * (cb * vb - ca * va)) - kw, - ka * kw])
+        roots = equation.roots
+    
