8ab012d8bfbf9065fb0a87e2c729fa8fb822ebf2
太良 桜井 <taira_sakurai@outlook.jp>
2025-05-04 10:00:55 +0900

    Completed the program

--- TitrationPlotter/TitrationPlotter.py
+++ TitrationPlotter/TitrationPlotter.py
@@ -1,4 +1,5 @@
-﻿from math import nan
+﻿from csv import reader
+from math import nan
 import numpy as np
 import scipy.optimize as opt
 import matplotlib.pyplot as plt
@@ -8,7 +9,6 @@ from tkinter.ttk import Frame, Entry, Label, Spinbox, Button
 ca = 0.0
 cb = 0.0
 va = 110.0
-vb = 0.0
 kw = 1e-14
 root = Tk()
 root.title('設定')
@@ -17,23 +17,67 @@ caS = StringVar()
 kwS = StringVar()
 cbS = StringVar()
 
-def cmd ():
+def titration_pH (vb: np.ndarray, ka: float) -> np.ndarray:
+    '''This returns the array of the roots.
+
+    This function solves the equation about the concentration of proton.
+
+    The meaningd of the paramaters are:
+
+    ka: Ka value of the titrated acid.
+    vb: volume of added base.
+    '''
+    result = list()
+    print(ka)
+    # For each vb, make and solve the equation
+    for arr in np.nditer(vb):
+        print([1, ka + (arr * cb) / (va + arr), ((ka / (va + arr)) * (cb * arr - ca * va)) - kw, - ka * kw])
+        equation = np.poly1d([1, ka + (arr * cb) / (va + arr), ((ka / (va + arr)) * (cb * arr - ca * va)) - kw, - ka * kw])
+        roots = equation.roots
+        print(roots)
+        # Find suitable solution
+        result.append(np.max(roots))
+    print('result=',result)
+    rarray = -np.log10(np.array(result))
+    print(rarray)
+    return rarray
+
+def cmd () -> bool:
     '''Button command
     '''
-    pass
+    global root,ca,cb,kw
+    # Get values
+    # read csv file
+    pH = list()
+    vb = list()
+    with open(csvfilename.get(), 'r') as file:
+        r = reader(file)
+        for line in r:
+            vb.append(float(line[0]))
+            pH.append(float(line[1]))
+    vb = np.array(vb)
+    pH = np.array(pH)
+    ca = float(caS.get())
+    cb = float(cbS.get())
+    kw = float(kwS.get())
+    (popt, pcov) = opt.curve_fit(titration_pH, vb, pH)
+    xrange = np.arange(min(*vb),max(*vb),0.005)
+    plt.plot(xrange, titration_pH(xrange, *popt))
+    plt.show()
+    return False
 
 def settings () -> None:
     '''This sets the options of the plot
 
     This has no argumanets.
     '''
-    global ca,cb,kw,root,caS,cbS,kwS,csvfilename
+    global root,caS,cbS,kwS,csvfilename
     content = Frame(root)
     # CSV file of the data
     datainput = Entry(content, textvariable=csvfilename)
     datalabel = Label(content, text='滴定データを含むcsvファイルのパス')
     # concentration of the acid
-    caEntry = Spinbox(content, from_=0.00, to=10.000, increment=0.0001, textvariable=caS)
+    caEntry = Spinbox(content, from_=0.00, to=10.000, increment=0.000001, textvariable=caS)
     caLabel = Label(content, text='被滴定溶液の容量モル濃度 / M')
     # Kw
     kwEntry = Entry(content, textvariable=kwS)
@@ -42,7 +86,7 @@ def settings () -> None:
     cbEntry = Entry(content, textvariable=cbS)
     cbLabel = Label(content, text='滴定時に加えた塩基の濃度/ mol L-1')
     # Button
-    btn = Button(content, text='プロットを作成')
+    btn = Button(content, text='プロットを作成', command=cmd)
     content.grid(column=0, row=0)
     datainput.grid(column=1, row=0, columnspan=2)
     datalabel.grid(column=0, row=0)
@@ -57,32 +101,3 @@ def settings () -> None:
     return None
 
 settings()
-
-def titration_pH (vb: np.ndarray, ka: float) -> np.ndarray:
-    '''This returns the array of the roots.
-
-    This function solves the equation about the concentration of proton.
-
-    The meaningd of the paramaters are:
-
-    ka: Ka value of the titrated acid.
-    ca: initial concentration of the concentration of the titrated acid.
-    va: Initial volume of the acid.
-    cb: initial concentration of the concentration of the titrating base.
-    vb: volume of added base.
-    kw: Kw value at the tempreture.
-    '''
-    result = list()
-    # For each vb, make and solve the equation
-    for arr in np.nditer(vb):
-        equation = np.poly1d([1, ka + (vb * cb) / (va + vb), ((ka / (va + vb)) * (cb * vb - ca * va)) - kw, - ka * kw])
-        roots = equation.roots
-        # Find suitable solution
-        for root in np.nditer(roots):
-            if type(root) == float:
-                if root > 0:
-                    result.append(root)
-                    break
-                else:
-                    continue
-    return np.array(result)
