7ecf8b99bf3b03f2d4167094906331ed88a7c0bd
太良 桜井 <taira_sakurai@outlook.jp>
2025-05-04 13:45:47 +0900

    Debugged

--- TitrationPlotter/TitrationPlotter.py
+++ TitrationPlotter/TitrationPlotter.py
@@ -1,23 +1,29 @@
 ﻿from csv import reader
-from math import nan
+from math import ceil, nan
 import numpy as np
 import scipy.optimize as opt
 import matplotlib.pyplot as plt
-from tkinter import Button, Widget, PhotoImage, Tk, StringVar, BooleanVar
+from tkinter import Button, Listbox, Widget, PhotoImage, Tk, StringVar, BooleanVar
 from tkinter.ttk import Frame, Entry, Label, Spinbox, Button
 
 ca = 0.0
 cb = 0.0
 va = 110.0
 kw = 1e-14
+k1 = 4.335e-7
+k2 = 1.982e-17
 root = Tk()
 root.title('設定')
+content = Frame(root)
 csvfilename = StringVar()
 caS = StringVar()
 kwS = StringVar()
 cbS = StringVar()
-
-def titration_pH (vb: np.ndarray, ka: float) -> np.ndarray:
+ylabel = StringVar()
+xlabel = StringVar()
+# Result
+resultbox = Listbox(content, height=3, width=50)
+def titration_pH (vb: np.ndarray, ka: float, delta1: float, delta2: float) -> np.ndarray:
     '''This returns the array of the roots.
 
     This function solves the equation about the concentration of proton.
@@ -26,17 +32,19 @@ def titration_pH (vb: np.ndarray, ka: float) -> np.ndarray:
 
     ka: Ka value of the titrated acid.
     vb: volume of added base.
+    delta1: correction of concentration of proton
+    delta2: correction
     '''
     result = list()
     print(ka)
     # For each vb, make and solve the equation
     for arr in np.nditer(vb):
-        print([1, ka + (arr * cb) / (va + arr), ((ka / (va + arr)) * (cb * arr - ca * va)) - kw, - ka * kw])
-        equation = np.poly1d([1, ka + (arr * cb) / (va + arr), ((ka / (va + arr)) * (cb * arr - ca * va)) - kw, - ka * kw])
-        roots = equation.roots
-        print(roots)
-        # Find suitable solution
-        result.append(np.max(roots))
+        delta = delta1 * arr + delta2
+        polyn = [1, ka + (arr * cb) / (va + arr), ((ka / (va + arr)) * (cb * arr - ca * va)) - kw - delta * k1, - (ka * kw + 2 * delta * k2 + delta * ka * k1), -2 * k2 * ka]
+        polyn.reverse()
+        f = lambda x: np.polynomial.polynomial.polyval(x, polyn)
+        rf = opt.fsolve(f, [0.1])
+        result.append(rf[0])
     print('result=',result)
     rarray = -np.log10(np.array(result))
     print(rarray)
@@ -60,9 +68,20 @@ def cmd () -> bool:
     ca = float(caS.get())
     cb = float(cbS.get())
     kw = float(kwS.get())
-    (popt, pcov) = opt.curve_fit(titration_pH, vb, pH)
-    xrange = np.arange(min(*vb),max(*vb),0.005)
+    (popt, pcov) = opt.curve_fit(titration_pH, vb, pH, (1.754e-5, 0, 1.31e-5))
+    for p in np.nditer(popt):
+        resultbox.insert('end', p)
+    xrange = np.arange(min(*vb),max(*vb),0.00005)
     plt.plot(xrange, titration_pH(xrange, *popt))
+    plt.plot(vb,pH,'.')
+    plt.grid(True, 'both')
+    plt.minorticks_on()
+    plt.xticks(np.arange(int(min(vb)), ceil(max(vb)), 0.02), minor=True)
+    plt.yticks(np.arange(int(min(pH)), ceil(max(pH)), 0.05), minor=False)
+    plt.xticks(np.arange(int(min(vb)), ceil(max(vb))), minor=True)
+    plt.yticks(np.arange(int(min(pH)), ceil(max(pH))), minor=False)
+    plt.ylabel(ylabel.get())
+    plt.xlabel(xlabel.get())
     plt.show()
     return False
 
@@ -71,8 +90,7 @@ def settings () -> None:
 
     This has no argumanets.
     '''
-    global root,caS,cbS,kwS,csvfilename
-    content = Frame(root)
+    global root,caS,cbS,kwS,csvfilename,xlabel,ylabel,graphlabel
     # CSV file of the data
     datainput = Entry(content, textvariable=csvfilename)
     datalabel = Label(content, text='滴定データを含むcsvファイルのパス')
@@ -85,6 +103,12 @@ def settings () -> None:
     # Cb
     cbEntry = Entry(content, textvariable=cbS)
     cbLabel = Label(content, text='滴定時に加えた塩基の濃度/ mol L-1')
+    # X label
+    xLabelEntry = Entry(content, textvariable=xlabel)
+    xLabelLabel = Label(content, text='x軸のラベル')
+    # Y label
+    yLabelEntry = Entry(content, textvariable=ylabel)
+    yLabelLabel = Label(content, text='y軸のラベル')
     # Button
     btn = Button(content, text='プロットを作成', command=cmd)
     content.grid(column=0, row=0)
@@ -96,7 +120,12 @@ def settings () -> None:
     cbLabel.grid(column=0, row=2)
     kwEntry.grid(column=1, row=3, columnspan=2)
     kwLabel.grid(column=0, row=3)
-    btn.grid(column=2, row=4)
+    xLabelEntry.grid(column=1, row=4, columnspan=2)
+    xLabelLabel.grid(column=0, row=4)
+    yLabelEntry.grid(column=1, row=5, columnspan=2)
+    yLabelLabel.grid(column=0, row=5)
+    resultbox.grid(column=0,row=6,columnspan=3)
+    btn.grid(column=2, row=7)
     root.mainloop()
     return None
 
