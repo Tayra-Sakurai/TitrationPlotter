65dea78c7be2a85a345aeca343966a10b979bda8
太良 桜井 <taira_sakurai@outlook.jp>
2025-05-05 20:59:40 +0900

    Added a license notice.

--- TitrationPlotter/TitrationPlotter.py
+++ TitrationPlotter/TitrationPlotter.py
@@ -1,4 +1,13 @@
-﻿from csv import reader
+﻿#    This program is a titration plotter. This is TitrationPlotter.py written in Python 3.13.2.
+#    Copyright (C) 2025 Tayra Sakurai
+#
+#    This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+#
+#    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+from csv import reader
 from math import ceil, log10, nan
 from sys import version
 import numpy as np
